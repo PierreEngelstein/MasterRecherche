08a9e02c1026e9ce814adeac9ea38e9807cdd4c5
nico <nico.33@free.fr>
2020-10-20 16:50:11 +0200

    Nicolas Delanoue a fait des modif sur DJ

---
+++ Deutsch-Jozsa/denombrement.py
@@ -0,0 +1,15 @@
+import math
+n = 8
+N = math.factorial(2**n)
+R = math.factorial(2**(n-1))
+D=R**2
+equilibree = N/D
+total = 2**(2**n)
+proportion = equilibree / total
+print("equilibree : " + str(equilibree))
+print("total : " + str(total))
+print("proportion : " + str(proportion))
+
+
+
+
