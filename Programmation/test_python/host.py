933548899a99a653cdbdb7b209bde78b5104bdb5
Pierre Engelstein <pierre.engelstein@productys.com>
2020-12-14 08:30:42 +0100

    Start week 51

--- Programmation/test_python/host.py
+++ Programmation/test_python/host.py
@@ -3,7 +3,9 @@
 from qiskit.visualization import plot_bloch_vector, plot_state_qsphere, plot_bloch_multivector
 from qiskit.quantum_info import Statevector
 from qiskit import QuantumCircuit, execute, QuantumRegister, ClassicalRegister, Aer
+from qiskit.circuit.library import QFT
 import matplotlib.pyplot as plt
+import numpy as np
 
 def show_figure(fig):
     new_fig = plt.figure()
@@ -22,12 +24,14 @@ def DJ_StepByStep(n, oracle, title, multivector=True):
     circuit_init = QuantumCircuit(n+1)
     circuit_init.x(n)
 
+    # Create initial state (|00 ... 01>)
     u0 = u0.evolve(circuit_init)
     if multivector == True:
         fig_u0 = plot_bloch_multivector(u0, title="|u0>")
     else:
         fig_u0 = plot_state_qsphere(u0.data)
 
+    # Apply hadamard gate to all qubits
     circuit_hadamard_in = QuantumCircuit(n+1)
     for i in range(n+1):
         circuit_hadamard_in.h(i)
@@ -37,17 +41,18 @@ def DJ_StepByStep(n, oracle, title, multivector=True):
     else:
         fig_u1 = plot_state_qsphere(u1.data)
 
+    # Apply the oracle
     u2 = u1.evolve(oracle)
     if multivector == True:
         fig_u2 = plot_bloch_multivector(u2, title="|u2> = Uf|u1>, " + title)
     else:
         fig_u2 = plot_state_qsphere(u2.data)
 
+    # Apply hadamard
     circuit_hadamard_out = QuantumCircuit(n+1)
     for i in range(n):
         circuit_hadamard_out.h(i)
     u3 = u2.evolve(circuit_hadamard_out)
-
     if multivector == True:
         fig_u3 = plot_bloch_multivector(u3, title="|u3> = H|u2>")
     else:
@@ -56,39 +61,62 @@ def DJ_StepByStep(n, oracle, title, multivector=True):
     return fig_u0
 
 
-myOracle = QuantumCircuit(4)
-myOracle.z(0)
-myOracle.z(2)
-fig = DJ_StepByStep(4, myOracle, "Uf = z(0) et z(2)", False)
-show_figure(fig)
-
-# n = 5
-# circuit = QuantumCircuit(n+1, n)
-
-# circuit.x(n) # Put last qubit to 1
-
-# circuit.barrier()
-
-# for qubit in range(n):
-#     circuit.h(qubit)
-
-# circuit.h(n)
-
-# circuit.barrier()
-# circuit.z(0)
-# circuit.z(1)
-# circuit.z(4)
-# circuit.barrier()
-
-# for qubit in range(n):
-#     circuit.h(qubit)
-
-# circuit.barrier()
-
-# for i in range(n):
-#     circuit.measure(i, i)
-
-# fig = circuit.draw('mpl')
+#myOracle = QuantumCircuit(4)
+# myOracle.z(0)
+# myOracle.z(2)
+# fig = DJ_StepByStep(4, myOracle, "Uf = z(0) et z(2)", False)
 # show_figure(fig)
 
-# print(execute(circuit, Aer.get_backend("qasm_simulator")).result().get_counts())
+init_state = '000'
+u0 = Statevector.from_label(init_state)
+#fig = plot_bloch_multivector(u0, title="QFT transform")
+test_qft = QFT(3)
+u1=u0.evolve(test_qft)
+fig = plot_bloch_multivector(u1, title="QFT transform")
+
+init_state = '001'
+u0 = Statevector.from_label(init_state)
+#fig = plot_bloch_multivector(u0, title="QFT transform")
+test_qft = QFT(3)
+u1=u0.evolve(test_qft)
+fig = plot_bloch_multivector(u1, title="QFT transform")
+
+init_state = '010'
+u0 = Statevector.from_label(init_state)
+test_qft = QFT(3)
+u1=u0.evolve(test_qft)
+fig = plot_bloch_multivector(u1, title="QFT transform")
+
+init_state = '011'
+u0 = Statevector.from_label(init_state)
+test_qft = QFT(3)
+u1=u0.evolve(test_qft)
+fig = plot_bloch_multivector(u1, title="QFT transform")
+
+init_state = '100'
+u0 = Statevector.from_label(init_state)
+#fig = plot_bloch_multivector(u0, title="QFT transform")
+test_qft = QFT(3)
+u1=u0.evolve(test_qft)
+fig = plot_bloch_multivector(u1, title="QFT transform")
+
+init_state = '101'
+u0 = Statevector.from_label(init_state)
+#fig = plot_bloch_multivector(u0, title="QFT transform")
+test_qft = QFT(3)
+u1=u0.evolve(test_qft)
+fig = plot_bloch_multivector(u1, title="QFT transform")
+
+init_state = '110'
+u0 = Statevector.from_label(init_state)
+test_qft = QFT(3)
+u1=u0.evolve(test_qft)
+fig = plot_bloch_multivector(u1, title="QFT transform")
+
+init_state = '111'
+u0 = Statevector.from_label(init_state)
+test_qft = QFT(3)
+u1=u0.evolve(test_qft)
+fig = plot_bloch_multivector(u1, title="QFT transform")
+
+show_figure(fig)
