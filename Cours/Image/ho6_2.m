8cb2ffa06280af702c835c1cf6c82b4a930a6ed7
pierre <pierre@pop-os.localdomain>
2021-03-28 10:01:37 +0200

    Removed cours

--- Cours/Image/ho6_2.m
+++
@@ -1,6 +0,0 @@
-pkg load image
-i = imread('holmes_nb.bmp');
-imshow(i);
-
-J = imnoise(i, 'salt & pepper', 0.5);
-imshow(i);
--- Cours/interval_analysis/main_TD0.py
+++
@@ -1,9 +0,0 @@
-from interval.interval import Interval
-
-i1 = Interval(10, 3)
-i2 = Interval(7, 9)
-
-print(str(i1) + " + " + str(i2) + " = " + str(i1+i2))
-print(str(i1) + " - " + str(i2) + " = " + str(i1-i2))
-print(str(i1) + " / " + str(i2) + " = " + str(i1/i2))
-print(str(i1) + " * " + str(i2) + " = " + str(i1*i2))
